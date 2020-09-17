4035a473f5fd6839551173cf6507b3d950efe74d
yudenichaa <yudenichalexandr@yandex.ru>
2020-09-17 03:24:29 +0300

    fix :)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
 					/>
 				</div>
 				<div className="Header-Contacts">
-					<p className="Header-Info">Кадастровый инженер ИП Юденич А. А.</p>
+					<p className="Header-Info">Кадастровый инженер ИП Юденич А. И.</p>
 					<p className="Header-Info">+7 (978) 834-31-76 (Светлана)</p>
 					<p className="Header-Info Header-Info_mdMb_0">+7 (978) 823-89-15 (Анатолий)</p>
 				</div>
